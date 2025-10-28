4ee7d640953b8b3b7f1b8ac95b2fb5714f66a836
Pravinkumaar P <p.pravinkumaar@atgeirsolutions.com>
2025-10-28 11:47:21 +0000

    commit2

--- models/example/my_second_dbt_model.sql
+++ models/example/my_second_dbt_model.sql
@@ -1,15 +1,6 @@
 
 -- Use the `ref` function to select from other models
 
---select *
---from {{ ref('my_first_dbt_model') }}
---where id = 1
-
-
-{{ config(materialized='table') }}
-
-with cte as 
-(
-    select * from dbt_ds.raw_customers
-)
-select * from cte
+select *
+from {{ ref('my_first_dbt_model') }}
+where id = 1
